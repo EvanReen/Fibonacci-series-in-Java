c3a59aa72b31155db11a64e3f730ae7753b4c287
Evan Reen <74310324+EvanReen@users.noreply.github.com>
2022-06-29 15:45:56 +0100

    Update Fibonacci Series without using recursion.py

--- Fibonacci Series without using recursion.py
+++ Fibonacci Series without using recursion.py
@@ -3,7 +3,8 @@ class FibonacciNoRecursion{
  
     int n1=0,n2=1,n3,i,count=10;    
  
-    System.out.print(n1+" "+n2);//printing 0 and 1    
+    //printing 0 and 1 
+    System.out.print(n1+" "+n2);   
     
     //loop starts from 2 because 0 and 1 are already printed
     for(i=2;i<count;++i) {
